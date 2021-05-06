bb96e354a0cf02f4eeafadb1eedf349402ad2316
AnnLemottien <75042923+AnnLemottien@users.noreply.github.com>
2021-05-06 20:39:44 +0200

    Delete mobile-nav.js

--- JS/mobile-nav.js
+++
@@ -1,4 +0,0 @@
-function tooglemenu(){         
-    document.querySelector('.mobile-menu').classList.toggle('opened');
-}
-document.querySelector('.mobile-menu-icon').addEventListener('click', tooglemenu);
